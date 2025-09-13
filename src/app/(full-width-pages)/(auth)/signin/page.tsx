81bcf49b3f6ac415e265ccf648e4b91831bfd4f7
gaida-lisongo <inbtpkinshasa@gmail.com>
2025-09-13 06:33:33 +0000

    change data

--- src/app/(full-width-pages)/(auth)/signin/page.tsx
+++ src/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -2,7 +2,7 @@ import SignInForm from "@/components/auth/SignInForm";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Connexion AAdmin",
+  title: "Connexion Admin",
   description: "Page de connexion pour les agents de l'institut. Authentification sécurisée avec matricule et mot de passe.",
 };
 
