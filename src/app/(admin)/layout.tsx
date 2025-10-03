e80dba4b2ef0affe3255cb89fee4e7d97ffd8721
gaida-lisongo <inbtpkinshasa@gmail.com>
2025-10-03 05:36:09 +0100

    menu ok

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -23,11 +23,11 @@ export default function AdminLayout({
   return (
     <div className="min-h-screen xl:flex">
       {/* Sidebar and Backdrop */}
-      <AppSidebar />
-      <Backdrop />
+      {/* <AppSidebar /> */}
+      {/* <Backdrop /> */}
       {/* Main Content Area */}
       <div
-        className={`flex-1 transition-all  duration-300 ease-in-out ${mainContentMargin}`}
+        className={`flex-1 transition-all  duration-300 ease-in-out`}
       >
         {/* Header */}
         <AppHeader />
