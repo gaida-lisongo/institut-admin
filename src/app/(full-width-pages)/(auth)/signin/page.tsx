b165cbb740188967acae5f020f85b129fa1faaea
gaida-lisongo <inbtpkinshasa@gmail.com>
2025-10-05 23:12:21 +0100

    fixe resultat 4

--- src/app/(full-width-pages)/(auth)/signin/page.tsx
+++ src/app/(full-width-pages)/(auth)/signin/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function SignIn() {
   return (
-    <div className="lg:w-1/2 w-full h-full flex flex-col justify-center px-4 py-12 sm:px-6 lg:px-20 xl:px-24">
+    <div className="w-full min-h-screen flex flex-col justify-center px-4 py-12 sm:px-6 lg:px-20 xl:px-24">
       <LoginForm />
     </div>
   );
